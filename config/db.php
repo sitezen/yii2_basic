7c5b0225fb4413a0d0efb1b40a5e207781badafa
alexanderK <alexander.kir@mail.ru>
2018-08-17 15:16:36 +0300

    Description of project added Docker implementation added

--- config/db.php
+++ config/db.php
@@ -1,27 +1,11 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cbt',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'class'      =>'yii\db\Connection',
+    'dsn'        =>'mysql:host=yktoo-db;dbname=yktoo',
+    'username'   =>'appuser',
+    'password'   =>'appuserPasswd',
+    'charset'    =>'utf8',
+    'tablePrefix'=>'t_',
 ];
-/*
-'db1' => [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cbt',
-    'username' => 'db1username',
-    'password' => 'db1password',
-],
-    'db2' => [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=nemo_guide_etalon',
-    'username' => 'db2username',
-    'password' => 'db2password',
-],*/
+?>
